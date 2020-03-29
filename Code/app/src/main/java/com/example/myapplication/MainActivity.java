7889acc87c0a2fc66cf366bbdc122f05abf738c4
Avijeet10 <57415095+Avijeet10@users.noreply.github.com>
2020-03-29 16:35:24 -0500

    Added classes Login and MainActivity their xml and some drawable designs for LoginController.

--- Code/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Code/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button starts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        starts = (Button) findViewById(R.id.started);
+
+        starts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+
+            }
+        });
+
+
+
+
     }
+
 }
