caf002885ef2d4933e98906108ad2fea7af8667d
Bijay Raj Raut <51686520+shreroa@users.noreply.github.com>
2020-03-31 13:20:32 -0500

    Sign Up Screen (Simplified than Mock Up)

--- Code/app/src/main/java/com/example/myapplication/Login.java
+++ Code/app/src/main/java/com/example/myapplication/Login.java
@@ -2,13 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private TextView Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Register = (TextView)findViewById(R.id.register);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,UserRegistration.class));
+            }
+        });
+
     }
 }
