8dc50dea7eb4d281975188d261bf12b9373ecb6b
Avijeet10 <57415095+Avijeet10@users.noreply.github.com>
2020-04-08 14:27:20 -0500

    Added classes Login and MainActivity their xml and some drawable designs for LoginController.

---
+++ Code/app/src/main/java/com/example/myapplication/Workedout_user_homescreen.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Workedout_user_homescreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workedout_user_homescreen);
+    }
+}
