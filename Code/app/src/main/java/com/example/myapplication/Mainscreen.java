f1e18efb77e944c419d33ce82d8cedac2461763b
Bijay Raj Raut <51686520+shreroa@users.noreply.github.com>
2020-04-08 22:54:40 -0500

    setting and profile page created (updated)

--- Code/app/src/main/java/com/example/myapplication/Mainscreen.java
+++ Code/app/src/main/java/com/example/myapplication/Mainscreen.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Mainscreen extends AppCompatActivity {
@@ -12,6 +14,17 @@ public class Mainscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
+
+        final Button tempbutton= (Button) findViewById(R.id.tempbutton);
+
+        tempbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(Mainscreen.this, ProfilePage.class));
+            }
+        });
+
     }
 
 }
