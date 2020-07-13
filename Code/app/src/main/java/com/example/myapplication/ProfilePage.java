756e8639c4cc7419499394c5e876805558336a47
Sudeep <sanam@Sanam.localdomain>
2020-07-13 16:24:36 -0500

    Made changes on Profile and Program Page

--- Code/app/src/main/java/com/example/myapplication/ProfilePage.java
+++ Code/app/src/main/java/com/example/myapplication/ProfilePage.java
@@ -2,13 +2,71 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.softmoore.android.graphlib.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
+
 
 public class ProfilePage extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        LineChartView lineChartView = findViewById(R.id.showGraph);
+        String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
+                "Oct", "Nov", "Dec"};
+        int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#121493"));;
+        for(int i = 0; i < axisData.length; i++){
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++){
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+        List lines = new ArrayList();
+        lines.add(line);
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        lineChartView.setLineChartData(data);
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        data.setAxisYLeft(yAxis);
+
+        axis.setTextSize(15);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+
+
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top =110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
+
+
+
     }
+
 }
